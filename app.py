835b9de065f4b5ed045829616206f25f4556a37b
MariaSierra <66576032+mariasierralizarazo@users.noreply.github.com>
2020-11-12 23:15:04 -0500

    App code modification - conecting to MongoDB

--- app.py
+++ app.py
@@ -4,20 +4,21 @@ import scrape_mars
 
 app = Flask(__name__)
 
-conn = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route('/')
 def index_page():
-    data = conn.mars_db.mars_collection.find_one()
-    return render_template("index.html", data=data)
+    mars_data = mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_data=mars_data)
 
 @app.route('/scrape')
 def scrape():
-    mars_data = scrape_mars.scrape()
-    conn.mars_db.mars_collection.update({}, mars_data, upsert = True)
-    return redirect("/")
+    mars_data = mongo.db.mars_data
+    mars_info = scrape_mars.scrape()
+    mars_data.update({}, mars_info, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
-    spp.run(debug=True)
+    app.run(debug=True)
 
 
